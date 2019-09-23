7b89e5d320fa816628e7fc85be41e9cbc0167ce5
Paul <paulcatinean@gmail.com>
2019-09-23 18:15:54 +0200

    Added attrs and relevant attr information to product.template cached json

--- mydisplays_configurator/models/product_config.py
+++ mydisplays_configurator/models/product_config.py
@@ -35,6 +35,7 @@ class ProductConfigSession(models.Model):
             'session': {
                 'price': 0,
                 'weight': 0,
+                'quantity': 0,
             },
             'config': config,
         }
