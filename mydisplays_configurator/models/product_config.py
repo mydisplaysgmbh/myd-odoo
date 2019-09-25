d24d830cfdf7c1b50ac47b2406d766fc06d920a1
Shruti Singh <shruti.singh@bizzappdev.com>
2019-09-25 17:24:09 +0530

    [FIX]remove extra code and store only custom values on config json

--- mydisplays_configurator/models/product_config.py
+++ mydisplays_configurator/models/product_config.py
@@ -24,14 +24,15 @@ class ProductConfigSession(models.Model):
             config[attr_json_name] = tmpl_config_cache["attr_vals"][val_id]
 
         # Add custom value_ids to config dict using human readable json name
-        for attr_json_name, vals in self.json_config.get("attrs", {}).items():
+        custom_val_id = self.get_custom_value_id()
+        for attr_id, vals in self.json_config.items():
             if not vals.get("value", False):
                 continue
             value = vals.get("value")
             # val_id = str(attr_val.id)
             # json_val = tmpl_config_cache["attr_vals"].get(val_id, {})
             # attr_id = json_val.get("attribute_id")
-            # attr_json_name = tmpl_config_cache["attr_json_map"][str(attr_id)]
+            attr_json_name = tmpl_config_cache["attr_json_map"][str(attr_id)]
             # TODO: Add typecast using custom_type info
             config[attr_json_name] = value
 
@@ -86,32 +87,8 @@ class ProductConfigSession(models.Model):
                 return float(val)
             except Exception:
                 raise UserError(_("Please provide valid float value"))
-        elif custom_type in [
-            "text",
-            "char",
-            "color",
-            "binary",
-            "date",
-            "datetime",
-        ]:
-            return val
         else:
             return val
-
-    @api.model
-    def get_default_json_dict(self, product_tmpl_id=None):
-        """Create basic structure for config JSON field"""
-        cfg_session_json = {}
-        if not product_tmpl_id:
-            product_tmpl_id = self.product_tmpl_id
-        tmpl_config_cache = product_tmpl_id.config_cache
-        attr_json_map = tmpl_config_cache.get("attr_json_map", {})
-        attrs = tmpl_config_cache.get("attrs", {})
-        cfg_session_json["attrs"] = {}
-        for attribute_id in attrs:
-            attr_json_name = attr_json_map.get(attribute_id, attribute_id)
-            cfg_session_json["attrs"][attr_json_name] = {}
-        return cfg_session_json
 
     @api.multi
     def get_configuration_session_json_dictionary(
@@ -120,16 +97,8 @@ class ProductConfigSession(models.Model):
         """Store product.config.session data in serialized computed field
             {
                 'attrs': {
-                    attr_1_JSON_name: {
+                    attr_1_id: {
                         'value': custom - value,  # (sanitized and typecasted),
-                        # (when standard option and attr_val_id is selected,
-                        # cannot be both custom and value_id)
-                        'attribute_id': attr_1_id,
-                        'value_id': attr_val_id,
-                        'product': attr_val.product_id.id,
-                        'price': attr_val.product_id.price,
-                        'weight': attr_val.product_id.weight,
-                        'quantity': product_quantity (computed value)
                     }
                 }
             }
@@ -139,9 +108,7 @@ class ProductConfigSession(models.Model):
         if not product_tmpl_id:
             product_tmpl_id = self.product_tmpl_id
         tmpl_config_cache = product_tmpl_id.config_cache
-        attr_json_map = tmpl_config_cache.get("attr_json_map", {})
         attrs = tmpl_config_cache.get("attrs", {})
-        attr_vals = tmpl_config_cache.get("attr_vals", {})
         product_configurator_obj = self.env["product.configurator"]
         field_prefix = product_configurator_obj._prefixes.get("field_prefix")
         custom_field_prefix = product_configurator_obj._prefixes.get(
@@ -150,24 +117,20 @@ class ProductConfigSession(models.Model):
         custom_val_id = self.get_custom_value_id()
         cfg_session_json = self.json_config
         if not cfg_session_json:
-            self.get_default_json_dict(product_tmpl_id=product_tmpl_id)
+            cfg_session_json = {}
         for attribute_id in attrs:
-            attr_json_name = attr_json_map.get(attribute_id, attribute_id)
-            attr_dict = {'attribute_id': attribute_id}
-            field_name = "%s%s" % (field_prefix, attribute_id)
+            if not attrs[attribute_id].get("custom", False):
+                return
+            attr_dict = {}
             custom_field = "%s%s" % (custom_field_prefix, attribute_id)
-            if field_name not in vals:
+            if custom_field not in vals:
                 continue
-            value = vals.get(field_name, False)
+            value = vals.get(custom_field, False)
             if not value:
                 # If value removed
-                attr_dict = {'attribute_id': attribute_id}
-            elif (
-                attrs[attribute_id].get("custom", False)
-                and value == custom_val_id.id
-            ):
+                attr_dict = {}
+            else:
                 # Custom value
-                value = vals.get(custom_field, False)
                 custom_type = attrs.get(attribute_id, {}).get(
                     "custom_type", "char"
                 )
@@ -175,19 +138,7 @@ class ProductConfigSession(models.Model):
                     val=value, custom_type=custom_type
                 )
                 attr_dict["value"] = custom_val
-            elif value in attr_vals:
-                # Standard attribute value
-                attr_dict["value_id"] = value
-                value_tree = attr_vals.get("%s" % (value), {})
-                product_id = value_tree.get("product_id", 0)
-                if product_id:
-                    attr_dict["product"] = product_id
-                    attr_dict["price"] = value_tree.get("price", 0)
-                    attr_dict["weight"] = value_tree.get("weight", 0)
-                else:
-                    attr_dict["price"] = value_tree.get("price_extra", 0)
-                    attr_dict["weight"] = value_tree.get("weight_extra", 0)
-            cfg_session_json["attrs"][attr_json_name] = attr_dict
+            cfg_session_json[attribute_id] = attr_dict
         return cfg_session_json
 
     @api.multi
@@ -200,18 +151,6 @@ class ProductConfigSession(models.Model):
         cfg_session_json = self.get_configuration_session_json_dictionary(
             vals=vals, product_tmpl_id=product_tmpl_id
         )
+        print("cfg_session_json ",cfg_session_json)
         self.json_config = cfg_session_json
         self.json_config_text = pprint.pformat(cfg_session_json)
-
-    @api.model
-    def create(self, vals):
-        """Add default structure of config JSON field"""
-        if vals.get("product_tmpl_id") and not vals.get("json_config"):
-            product_tmpl_id = self.env["product.template"].browse(
-                vals.get("product_tmpl_id")
-            )
-            json_config = self.get_default_json_dict(
-                product_tmpl_id=product_tmpl_id
-            )
-            vals.update({"json_config": json_config})
-        return super(ProductConfigSession, self).create(vals)
