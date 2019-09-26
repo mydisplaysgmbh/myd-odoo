d819c0f10d42ef5a434eefd155c697fb53c88dd0
Shruti Singh <shruti.singh@bizzappdev.com>
2019-09-26 11:06:04 +0530

    [IMP][T-00546]link config session with sale order line

--- mydisplays_configurator/__manifest__.py
+++ mydisplays_configurator/__manifest__.py
@@ -14,11 +14,13 @@
         'base_sparse_field',
         'product_configurator',
         'website_product_configurator',
+        'product_configurator_sale',
     ],
     'data': [
         'views/product_attribute_view.xml',
         'views/product_config_view.xml',
         'views/product_view.xml',
+        'views/sale_view.xml',
     ],
     'demo': [
     ],
