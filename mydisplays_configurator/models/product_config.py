8e1e1519a51d00b1539e9733a41617228f820d56
Paul Catinean <pca@pledra.com>
2019-10-18 11:55:53 +0300

    Flake8 and removed unused value_ids dependency

--- mydisplays_configurator/models/product_config.py
+++ mydisplays_configurator/models/product_config.py
@@ -31,8 +31,13 @@ class ProductConfigSession(models.Model):
         # Configuration data (attr vals and custom vals) using json name
         config = {}
         # Add selected value_ids to config dict using human readable json name
-        custom_val_id = self.get_custom_value_id()
-        for attr_val in list(set(self.json_config.get('value_ids') or []) - set([custom_val_id.id])):
+        custom_val = self.get_custom_value_id()
+
+        json_value_ids = list(
+            set(self.json_config.get('value_ids') or []) - set([custom_val.id])
+        )
+
+        for attr_val in json_value_ids:
             val_id = str(attr_val)
             json_val = tmpl_config_cache["attr_vals"].get(val_id, {})
             attr_id = json_val.get("attribute_id")
@@ -56,7 +61,7 @@ class ProductConfigSession(models.Model):
         }
 
     @api.multi
-    @api.depends("product_tmpl_id.config_cache", "json_config", "value_ids")
+    @api.depends("product_tmpl_id.config_cache", "json_config")
     def _compute_json_vals(self):
         for session in self:
             code = session.product_tmpl_id.computed_vals_formula
