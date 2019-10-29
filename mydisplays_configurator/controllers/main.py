1b93519bb107f333b4696e39f05eda50c3b426fa
Paul Catinean <pca@pledra.com>
2019-10-29 18:12:25 +0100

    Using no _id convention for recordsets

--- mydisplays_configurator/controllers/main.py
+++ mydisplays_configurator/controllers/main.py
@@ -54,32 +54,32 @@ class MydisplaysConfigWebsiteSale(ProductConfigWebsiteSale):
         """Render product page of product_id"""
         if not config_session.exists():
             return request.render("website.404")
-        product_id = config_session.product_id
-        product_tmpl_id = config_session.product_tmpl_id
-        if not product_tmpl_id.exists():
+        product = config_session.product_id
+        product_tmpl = config_session.product_tmpl_id
+        if not product_tmpl.exists():
             return request.render("website.404")
 
         cfg_user_id = config_session.user_id
         user_id = request.env.user
 
         check_for_session = (
-            product_id.product_tmpl_id == product_tmpl_id and
+            product.product_tmpl_id == product_tmpl and
             cfg_user_id == user_id and
             config_session.state == 'done'
         ) and True or False
 
         if not check_for_session:
-            return request.redirect('/shop/product/%s' % slug(product_tmpl_id))
+            return request.redirect('/shop/product/%s' % slug(product_tmpl))
 
         vals = sorted(
-            product_id.attribute_value_ids,
+            product.attribute_value_ids,
             key=lambda obj: obj.attribute_id.sequence
         )
         custom_vals = config_session.json_config.get('custom_values', {})
         pricelist = get_pricelist()
-        if request.session['product_config_session'].get(product_tmpl_id.id):
+        if request.session['product_config_session'].get(product_tmpl.id):
             product_config_session = request.session['product_config_session']
-            del product_config_session[product_tmpl_id.id]
+            del product_config_session[product_tmpl.id]
             request.session['product_config_session'] = product_config_session
 
         try:
@@ -92,14 +92,14 @@ class MydisplaysConfigWebsiteSale(ProductConfigWebsiteSale):
             product_qty = 1
 
         values = {
-            'product_id': product_id,
-            'product_tmpl': product_tmpl_id,
+            'product_id': product,
+            'product_tmpl': product_tmpl,
             'config_session': config_session,
             'pricelist': pricelist,
             'product_qty': product_qty,
             'custom_vals': custom_vals,
             'json_vals': config_session.json_vals,
-            'attr_data': product_tmpl_id.config_cache.get('attrs', {}),
+            'attr_data': product_tmpl.config_cache.get('attrs', {}),
             'vals': vals,
         }
 
