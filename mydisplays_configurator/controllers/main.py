c2c12e4057c5c9e56a88be7af96cba5632ec7f9f
Paul <paulcatinean@gmail.com>
2019-12-03 18:07:26 +0100

    Using request instead of self in controller

--- mydisplays_configurator/controllers/main.py
+++ mydisplays_configurator/controllers/main.py
@@ -17,7 +17,7 @@ class MydisplaysConfigWebsiteSale(ProductConfigWebsiteSale):
         to live instance without disrupting views until we can adapt
         the design of the configurator to the theme"""
         parentClass = ProductConfigWebsiteSale
-        if self.env.user.id in [1, 2]:
+        if request.env.user.id in [1, 2]:
             parentClass = WebsiteSale
         return super(parentClass, self).product(
                 product, category, search, **kwargs
